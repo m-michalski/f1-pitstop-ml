3bf0a89026dc2382fb0541e7982a73a7d33abc74
m-michalski <281292@student.pwr.edu.pl>
2026-05-12 22:41:21 +0200

    Add int to float convert

--- data_transform.py
+++ data_transform.py
@@ -9,14 +9,14 @@ y = data[1:,-1]
 print(f'Nr of samples: {(X.shape)[0]}\nNr of features: {(X.shape)[1]}')
 print(f'Nr of labels: {(y.shape)[0]}')
 
-drivers = np.unique(X[1:,1])
-compounds = np.unique(X[1:,2])
-races = np.unique(X[1:,3])
-years = np.unique(X[1:,4])
+drivers = np.unique(X[:,1])
+compounds = np.unique(X[:,2])
+races = np.unique(X[:,3])
+years = np.unique(X[:,4])
 
 print(f'Nr of unique drivers: {(drivers.shape)[0]}\nUnique drivers:\n{drivers}\n')
 print(f'Nr of unique compounds: {(compounds.shape)[0]}\nUnique compounds:\n{compounds}\n')
 print(f'Nr of unique races: {(races.shape)[0]}\nUnique races:\n{races}\n')
 print(f'Nr of unique years: {(years.shape)[0]}\nUnique years:\n{years}\n')
 
-
+X_float = X[:,5:].astype(dtype="float")
