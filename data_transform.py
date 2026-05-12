10011bc0d4fe62b15600a9ee75e130972c65e148
m-michalski <281292@student.pwr.edu.pl>
2026-05-12 22:25:36 +0200

    Add more readable prints for X,y shape

--- data_transform.py
+++ data_transform.py
@@ -6,8 +6,8 @@ print(f'Shape of the data: {data.shape}\n')
 X = data[:,:-1]
 y = data[:,-1]
 
-print(X.shape)
-print(y.shape)
+print(f'Nr of samples: {(X.shape)[0]}\nNr of features: {(X.shape)[1]}')
+print(f'Nr of labels: {(y.shape)[0]}')
 
 drivers = np.unique(X[:,1])
 
