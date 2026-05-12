f6f7b01be3cfc3f3f296359de48524defb306af3
m-michalski <281292@student.pwr.edu.pl>
2026-05-12 22:21:03 +0200

    Add unique drivers

--- data_transform.py
+++ data_transform.py
@@ -2,3 +2,13 @@ import numpy as np
 
 data = np.loadtxt("data/train.csv", dtype="object", delimiter=",")
 print(f'Shape of the data: {data.shape}\n')
+
+X = data[:,:-1]
+y = data[:,-1]
+
+print(X.shape)
+print(y.shape)
+
+drivers = np.unique(X[:,1])
+
+print(f'Nr of unique drivers: {(drivers.shape)[0]}\nUnique drivers:\n{drivers}')
