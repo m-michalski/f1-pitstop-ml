0055fd843b53ab0dbcf756e8811ba51ddaaf7815
m-michalski <281292@student.pwr.edu.pl>
2026-05-12 22:30:08 +0200

    Fix X, y/Add print unique compounds

--- data_transform.py
+++ data_transform.py
@@ -3,12 +3,17 @@ import numpy as np
 data = np.loadtxt("data/train.csv", dtype="object", delimiter=",")
 print(f'Shape of the data: {data.shape}\n')
 
-X = data[:,:-1]
-y = data[:,-1]
+X = data[1:,:-1]
+y = data[1:,-1]
 
 print(f'Nr of samples: {(X.shape)[0]}\nNr of features: {(X.shape)[1]}')
 print(f'Nr of labels: {(y.shape)[0]}')
 
-drivers = np.unique(X[:,1])
+drivers = np.unique(X[1:,1])
+compounds = np.unique(X[1:,2])
 
-print(f'Nr of unique drivers: {(drivers.shape)[0]}\nUnique drivers:\n{drivers}')
+print(compounds)
+
+#print(f'Nr of unique drivers: {(drivers.shape)[0]}\nUnique drivers:\n{drivers}')
+#print(f'Nr of unique compounds: {(compounds.shape)[0]}\nUnique compounds:\n{compounds}')
+
